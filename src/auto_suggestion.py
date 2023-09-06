a72edaa373edbe2fbc60a0acdb6b5ba67cfc2d21
NecronWasTaken <joleyjoker14@gmail.com>
2023-09-06 12:02:05 +0800

    sc

---
+++ src/auto_suggestion.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+cities_codes = ["АР", "БН", "БӨ", "БР", "БУ", "ГА",
+                "ГС", "ДА", "ДГ", "ДО", "ДУ", "ЗА",
+                "НА", "ОР", "ӨВ", "ӨМ", "СБ", "СҮ",
+                "СЭ", "ТӨ", "УА", "УБ", "УН", "УЕ", "УВ",
+                "ХО", "ХӨ", "ХЦ", "ХЭ", "ЦА", "ЧО", "ЭТ"]
+
+set_of_cities_codes = set(cities_codes)
+
+cc1 = ['У', 'Г', 'Н', 'Ц', 'С', 'Ч', 'З',
+       'Э', 'Д', 'О', 'Т', 'Х', 'А', 'Ө', 'Б']
+cc2 = ['А', 'С', 'У', 'Э', 'Н', 'В', 'Ө', 'О',
+       'Г', 'Б', 'Р', 'М', 'Ц', 'Т', 'Е', 'Ү']
+
+alphabet = ["А", "Б", "В", "Г", "Д", "Е",
+            "Ё", "Ж", "З", "И", "Й", "К",
+            "Л", "М", "Н", "О", "Ө", "П",
+            "Р", "С", "Т", "У", "Ү", "Ф",
+            "Х", "Ц", "Ч", "Ш", "Щ", "Ъ",
+            "Ы", "Ь", "Э", "Ю", "Я"]
+
+numbers = ["0", "1", "2", "3", "4",
+           "5", "6", "7", "8", "9"]
+
+chars = numbers + alphabet
+
+dict_of_chars = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'А': 10, 'Б': 11, 'В': 12, 'Г': 13, 'Д': 14, 'Е': 15, 'Ё': 16, 'Ж': 17, 'З': 18, 'И': 19, 'Й': 20, 'К': 21,
+                 'Л': 22, 'М': 23, 'Н': 24, 'О': 25, 'Ө': 26, 'П': 27, 'Р': 28, 'С': 29, 'Т': 30, 'У': 31, 'Ү': 32, 'Ф': 33, 'Х': 34, 'Ц': 35, 'Ч': 36, 'Ш': 37, 'Щ': 38, 'Ъ': 39, 'Ы': 40, 'Ь': 41, 'Э': 42, 'Ю': 43, 'Я': 44}
+
+set_of_alphabet = set(alphabet)
+set_of_numbers = set(numbers)
+
+
+def partial_permutations(elements, length):
+    if length == 0:
+        return [[]]
+    else:
+        partials = []
+        for element in elements:
+            sub_partials = partial_permutations(elements, length - 1)
+            for sub_partial in sub_partials:
+                partials.append([element] + sub_partial)
+        return partials
+
+
+def plate_number_validation(plate: str) -> list[int] | None:
+    incorrect = []
+
+    if (len(plate) != 7):
+        print('Incorrect length of Plate Number')
+        return None
+
+    for i in range(7):
+        if plate[i] not in set_of_numbers and plate[i] not in set_of_alphabet:
+            incorrect.append(i+1)
+
+    if len(incorrect) > 0:
+        print('Incorrect symbols at: ')
+        print(incorrect)
+        return None
+
+    for i in range(4):
+        try:
+            int(plate[i])
+        except:
+            incorrect.append(i)
+
+    if (not plate[4:6] in set_of_cities_codes):
+        incorrect += [4, 5]
+
+    if (not plate[6] in set_of_alphabet):
+        incorrect.append(6)
+
+    return incorrect
+
+
+def find_closest_numbers(number: str, inc: list[int],
+                         dump_of_closest_chars_1: np.ndarray, dump_of_closest_distances_1: np.ndarray,
+                         dump_of_closest_chars_4: np.ndarray, dump_of_closest_distances_4: np.ndarray,
+                         dump_of_closest_chars_5: np.ndarray, dump_of_closest_distances_5: np.ndarray,
+                         dump_of_closest_chars_6: np.ndarray, dump_of_closest_distances_6: np.ndarray,
+                         dump_of_closest_chars_cc: np.ndarray, dump_of_closest_distances_cc: np.ndarray, dump_of_closest_chars_pairs: np.ndarray) -> str:
+    chars = numbers + alphabet
+    print("Find closest numbers to:", number)
+
+    if len(inc) == 0:
+        mins = []
+        chars = []
+        for i in range(4):
+            index = np.argmin(dump_of_closest_distances_1[int(number[i])])
+            chars.append(dump_of_closest_chars_1[int(number[i])][index])
+            mins.append(dump_of_closest_distances_1[int(number[i])][index])
+
+        index = np.argmin(
+            dump_of_closest_distances_4[dict_of_chars[number[4]]])
+        chars.append(dump_of_closest_chars_4[dict_of_chars[number[4]]][index])
+        mins.append(
+            dump_of_closest_distances_4[dict_of_chars[number[4]]][index])
+
+        index = np.argmin(
+            dump_of_closest_distances_5[dict_of_chars[number[5]]])
+        chars.append(dump_of_closest_chars_5[dict_of_chars[number[5]]][index])
+        mins.append(
+            dump_of_closest_distances_5[dict_of_chars[number[5]]][index])
+
+        index = np.argmin(
+            dump_of_closest_distances_6[dict_of_chars[number[6]]])
+        chars.append(dump_of_closest_chars_6[dict_of_chars[number[6]]][index])
+        mins.append(
+            dump_of_closest_distances_6[dict_of_chars[number[6]]][index])
+
+        closest = np.argpartition(mins, 3)
+
+        l = []
+        for i in range(3):
+            new_num = number[:closest[i]] + \
+                chars[closest[i]] + number[closest[i]+1:7]
+            l.append(new_num)
+
+        return ",".join(l)
+
+    partial_perms = partial_permutations([0, 1, 2], len(inc))
+
+    l = []
+    s = set()
+    for perm in partial_perms:
+        new_num = ''
+        distance = 0.0
+        k = 0
+        for i in range(4):
+            if i in inc:
+                new_num += dump_of_closest_chars_1[dict_of_chars[number[i]]][perm[k]]
+                distance += dump_of_closest_distances_1[dict_of_chars[number[i]]][perm[k]]
+                k += 1
+            else:
+                new_num += number[i]
+        if 4 in inc:
+            cc = dump_of_closest_chars_4[dict_of_chars[number[4]]][perm[k]] + dump_of_closest_chars_5[dict_of_chars[number[5]]][perm[k+1]]
+            index = np.where(dump_of_closest_chars_pairs==cc)[0][0]
+            new_num += dump_of_closest_chars_cc[index][perm[k]]
+            distance += dump_of_closest_distances_cc[index][perm[k]]
+            k += 2
+        else:
+            new_num += number[4] + number[5]
+        if 6 in inc:
+            index6 = dict_of_chars[number[6]]
+            new_num += dump_of_closest_chars_6[index6][perm[k]]
+            distance += dump_of_closest_distances_6[index6][perm[k]]
+            k += 1
+        else:
+            new_num += number[6]
+        if new_num not in s:
+            s.add(new_num)
+            l.append((new_num, distance))
+
+    return ','.join([a[0] for a in sorted(l, key=lambda a: a[1])[:3]])
--- src/main.py
+++ src/main.py
@@ -1,37 +1,12 @@
+from itertools import permutations
+from re import T
 import sqlite3
 from thefuzz import fuzz
 from thefuzz import process
 from PIL import Image, ImageDraw, ImageFont
-import cv2
 import numpy as np
-from scipy.spatial.distance import euclidean
 from skimage.feature import hog
-from skimage import exposure
-
-cities_codes = ["АР", "БН", "БӨ", "БР", "БУ", "ГА",
-                "ГС", "ДА", "ДГ", "ДО", "ДУ", "ЗА",
-                "НА", "ОР", "ӨВ", "ӨМ", "СБ", "СҮ",
-                "СЭ", "ТӨ", "УА", "УБ", "УН", "УЕ", "УВ",
-                "ХО", "ХӨ", "ХЦ", "ХЭ", "ЦА", "ЧО", "ЭТ"]
-
-set_of_cities_codes = set(cities_codes)
-
-cc1 = {'А': 0, 'Б': 1, 'Г': 3, 'Д': 4, 'З': 8, 'Н': 14, 'О': 15,
-       'Ө': 16, 'С': 19, 'Т': 20, 'У': 21, 'Х': 24, 'Ц': 25, 'Ч': 26, 'Э': 32}
-cc2 = {'А': 0, 'Б': 1, 'В': 2, 'Г': 3, 'Е': 5, 'М': 13, 'Н': 14, 'О': 15,
-       'Ө': 16, 'Р': 18, 'С': 19, 'Т': 20, 'У': 21, 'Ү': 22, 'Ц': 25, 'Э': 32}
-
-alphabet = ["А", "Б", "В", "Г", "Д", "Е",
-            "Ё", "Ж", "З", "И", "Й", "К",
-            "Л", "М", "Н", "О", "Ө", "П",
-            "Р", "С", "Т", "У", "Ү", "Ф",
-            "Х", "Ц", "Ч", "Ш", "Щ", "Ъ",
-            "Ы", "Ь", "Э", "Ю", "Я"]
-
-numbers = ["0", "1", "2", "3", "4",
-           "5", "6", "7", "8", "9"]
-
-set_of_alphabet = set(alphabet)
+from auto_suggestion import *
 
 
 def generate_combinations(cur: sqlite3.Cursor) -> list[str]:
@@ -44,16 +19,22 @@ def generate_combinations(cur: sqlite3.Cursor) -> list[str]:
     return ans
 
 
-def get_plate_numbers_from_DB() -> list:
+def save_generated_plate_numbers_to_DB() -> None:
     con = sqlite3.connect("data.db")
     cur = con.cursor()
-    # cur.execute("CREATE TABLE plateNumbers(id, plate_number)")
-    # ans = generate_combinations(cur)
-    # for i in range(len(ans)):
-    #     cur.execute("""
-    #                     INSERT INTO plateNumbers VALUES (?,?)
-    #                 """, (i,ans[i]))
-    # con.commit()
+    cur.execute("CREATE TABLE plateNumbers(id, plate_number)")
+    ans = generate_combinations(cur)
+    for i in range(len(ans)):
+        cur.execute("""
+                        INSERT INTO plateNumbers VALUES (?,?)
+                    """, (i, ans[i]))
+    con.commit()
+    return
+
+
+def get_plate_numbers_from_DB() -> list[tuple[int, str]]:
+    con = sqlite3.connect("data.db")
+    cur = con.cursor()
     res = cur.execute("SELECT * FROM plateNumbers")
     return res.fetchall()
 
@@ -74,28 +55,6 @@ def compute_hog(image) -> np.ndarray:
 
 def calculate_distance(feature1: np.ndarray, feature2: np.ndarray):
     return np.linalg.norm(feature1 - feature2)
-
-
-def plate_number_validation(plate: str) -> list[bool] | None:
-    mask = [True] * 7
-
-    if (len(plate) != 7):
-        print('Incorrect length of Plate Number')
-        return None
-
-    for i in range(4):
-        try:
-            int(plate[i])
-        except:
-            mask[i] = False
-
-    if (not plate[4:6] in set_of_cities_codes):
-        mask[4], mask[5] = False, False
-
-    if (not plate[6] in set_of_alphabet):
-        mask[6] = False
-
-    return mask
 
 
 def get_array_of_features(chars: list) -> list[np.ndarray]:
@@ -121,59 +80,125 @@ def create_dump_of_distances() -> None:
     return
 
 
-def get_closest_chars_and_distances(char: str, target_chars: list[str], count: int, target_chars_distances: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    idx = np.argpartition(target_chars_distances, count)
-    chars = np.array(target_chars)[idx[:count]]
-    distances = target_chars_distances[idx[:count]]
+def get_closest_chars_and_distances(char: str, target_chars: list[str], target_chars_distances: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    idx = np.argpartition([target_chars_distances[i] + 20 if char == target_chars[i]
+                          else target_chars_distances[i] for i in range(len(target_chars_distances))], 3)
+    chars = np.array(target_chars)[idx[:3]]
+    distances = target_chars_distances[idx[:3]]
 
     return (np.delete(chars, np.where(chars == char)), np.delete(distances, np.where(distances == 0)))
 
 
-def create_dump_of_closest_chars_and_distances(dump_of_distances: np.ndarray, target_chars: list[str], count: tuple[int, int], target_range: tuple[int, int], type_name: str) -> None:
+def create_dump_of_closest_chars_and_distances(dump_of_distances: np.ndarray, target_is_numbers: bool, target_list: list[str], type_name: str) -> None:
+    if target_is_numbers:
+        target_chars = numbers
+        range1, range2 = 0, 10
+    else:
+        target_chars = alphabet
+        range1, range2 = 10, len(dump_of_distances[0])
     chars = []
     distances = []
+    set_of_target_list = set(target_list)
+
     for i in range(10):
         closest = get_closest_chars_and_distances(
-            numbers[i], target_chars, count[0], dump_of_distances[i][target_range[0]:target_range[1]])
+            numbers[i], target_chars, dump_of_distances[i][range1:range2])
         chars.append(closest[0])
         distances.append(closest[1])
     for i in range(len(alphabet)):
+        arr = dump_of_distances[i+10][range1:range2]
         closest = get_closest_chars_and_distances(
-            alphabet[i], target_chars, count[1], dump_of_distances[i+10][target_range[0]:target_range[1]])
+            alphabet[i], target_chars, np.array([arr[j] if alphabet[j] in set_of_target_list else arr[j]+10 for j in range(len(arr))]))
         chars.append(closest[0])
         distances.append(closest[1])
-    print(chars)
-    np.savetxt('dump_of_closest_chars_'+type_name+'.txt',
+
+    np.savetxt('dump_of_closest_chars_' + type_name + '.txt',
                np.array(chars), delimiter=" ", fmt="%s", encoding='utf-8')
     np.savetxt('dump_of_closest_distances_' +
                type_name+'.txt', np.array(distances))
     return
 
 
+def calculate_distance_between_pairs(dump_of_distances: np.ndarray, s1: str, s2: str) -> float:
+    distance = 0.0
+    for i in range(2):
+        if s1[i] != s2[i]:
+            distance += dump_of_distances[dict_of_chars[s1[i]]][dict_of_chars[s2[i]]]
+    return distance
+
+def create_dump_of_closest_cc(dump_of_distances: np.ndarray) -> None:
+    this_chars = []
+    distances = []
+    pairs = list(map( ''.join ,permutations(chars, 2))) + [chars[i] + chars[i] for i in range(len(chars))]
+    print(len(pairs))
+    for pair in pairs:
+        arr = []
+        for cc in cities_codes:
+            arr.append(calculate_distance_between_pairs(dump_of_distances, pair, cc))
+        closest = np.argpartition(arr, 4)[:3]
+
+        temp1 = [cities_codes[close] for close in closest]
+        temp2 = [arr[close] for close in closest]
+
+        this_chars.append(np.delete(temp1, np.where(temp1 == pair)))
+        distances.append(np.delete(temp2, np.where(temp2 == 0)))
+
+    np.savetxt('dump_of_closest_chars_pairs.txt',
+               np.array(pairs), delimiter=" ", fmt="%s", encoding='utf-8')
+    np.savetxt('dump_of_closest_chars_cc.txt',
+               np.array(this_chars), delimiter=" ", fmt="%s", encoding='utf-8')
+    np.savetxt('dump_of_closest_distances_cc.txt', np.array(distances))
+    return
+
 def main() -> None:
-    # plate_numbers = get_plate_numbers_from_DB()
-
-    # print(len(plate_numbers))
-    # print(plate_numbers[:10])
-
-    # print("Fuzz ration: ", fuzz.ratio("0000АРА", "0000АРА"))
-
     # create_dump_of_distances()
-    dump_of_distances = np.loadtxt('dump_of_distances.txt')
+    dump_of_distances: np.ndarray = np.loadtxt(
+        'dump_of_distances.txt', dtype="float") 
     # print(dump_of_distances)
-    create_dump_of_closest_chars_and_distances(
-        dump_of_distances, numbers, (4, 3), (0, 10), '1-4')
 
-    create_dump_of_closest_chars_and_distances(
-        dump_of_distances, alphabet, (3, 4), (10, len(numbers+alphabet)), '7')
-    dump_of_closest_chars = np.loadtxt(
+    # create_dump_of_closest_chars_and_distances(
+    #     dump_of_distances, True, alphabet ,'1-4')
+    # create_dump_of_closest_chars_and_distances(
+    #     dump_of_distances, False, cc1,'5')
+    # create_dump_of_closest_chars_and_distances(
+    #     dump_of_distances, False, cc2,'6')
+    # create_dump_of_closest_chars_and_distances(
+    #     dump_of_distances, False, alphabet,'7')
+    # create_dump_of_closest_cc(dump_of_distances)
+
+    dump_of_closest_chars_1 = np.loadtxt(
         'dump_of_closest_chars_1-4.txt', dtype=str, encoding='utf-8')
-    dump_of_closest_distances = np.loadtxt('dump_of_closest_distances_1-4.txt')
-    # print(dump_of_closest_chars)
-    # print(dump_of_closest_distances)
+    dump_of_closest_distances_1 = np.loadtxt(
+        'dump_of_closest_distances_1-4.txt')
 
+    dump_of_closest_chars_4 = np.loadtxt(
+        'dump_of_closest_chars_5.txt', dtype=str, encoding='utf-8')
+    dump_of_closest_distances_4 = np.loadtxt('dump_of_closest_distances_5.txt')
+
+    dump_of_closest_chars_5 = np.loadtxt(
+        'dump_of_closest_chars_6.txt', dtype=str, encoding='utf-8')
+    dump_of_closest_distances_5 = np.loadtxt('dump_of_closest_distances_6.txt')
+
+    dump_of_closest_chars_6 = np.loadtxt(
+        'dump_of_closest_chars_7.txt', dtype=str, encoding='utf-8')
+    dump_of_closest_distances_6 = np.loadtxt('dump_of_closest_distances_7.txt')
+
+    dump_of_closest_chars_cc = np.loadtxt(
+        'dump_of_closest_chars_cc.txt', dtype=str, encoding='utf-8')
+    dump_of_closest_distances_cc = np.loadtxt('dump_of_closest_distances_cc.txt')
+    dump_of_closest_chars_pairs = np.loadtxt(
+        'dump_of_closest_chars_pairs.txt', dtype=str, encoding='utf-8')
+    
+    number = 'ПОРС110'
+    inc = plate_number_validation(number)
+
+    if inc is not None:
+        print(find_closest_numbers(number, inc, dump_of_closest_chars_1, dump_of_closest_distances_1, 
+                                dump_of_closest_chars_4, dump_of_closest_distances_4, 
+                                dump_of_closest_chars_5, dump_of_closest_distances_5, 
+                                dump_of_closest_chars_6, dump_of_closest_distances_6,
+                                dump_of_closest_chars_cc, dump_of_closest_distances_cc, dump_of_closest_chars_pairs))
     print('...')
-
 
 if __name__ == '__main__':
     main()
